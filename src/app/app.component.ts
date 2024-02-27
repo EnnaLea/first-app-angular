4c20eb191631d526167a1ae628f28030e701f17a
EnnaLea <soleariaeve@gmail.com>
2024-02-27 18:55:37 +0100

    non funziona

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,18 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [
     HomeComponent, RouterOutlet, RouterModule],
-  template: `
-  <main>
-    <header class="brand-name">
-      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-    </header>
-    <section class="content">
-    <router-outlet></router-outlet>
-    </section>
-  </main>
-`,
+    template: `
+    <main>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+        </header>
+      </a>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
