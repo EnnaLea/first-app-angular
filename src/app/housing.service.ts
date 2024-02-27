0958bded8697c3a9334942940105a3a1a756a3b7
EnnaLea <soleariaeve@gmail.com>
2024-02-27 19:01:21 +0100

    update readme.md

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -1,37 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HousingLocation } from './housinglocation';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+// import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+// import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HousingService {
 
-  constructor(private http: HttpClient) {}
 
   url = 'http://localhost:3000/locations';
 
-  // async getAllHousingLocations(): Promise<HousingLocation[]> {
-  //   const data = await fetch(this.url);
-  //   return await data.json() ?? [];
-  // }
-
   async getAllHousingLocations(): Promise<HousingLocation[]> {
-    const data = await this.http.get<HousingLocation[]>(this.url).toPromise();
-    return data ?? [];
+    const data = await fetch(this.url);
+    return await data.json() ?? [];
   }
 
-  // async getHousingLocationById(id: number): Promise<HousingLocation | undefined> {
-  //   const data = await fetch(`${this.url}/${id}`);
-  //   return await data.json() ?? {};
-  // }
 
   async getHousingLocationById(id: number): Promise<HousingLocation | undefined> {
-    return this.http.get<HousingLocation>(`${this.url}/${id}`).pipe(
-      map(data => data ?? {})
-    ).toPromise();
+    const data = await fetch(`${this.url}/${id}`);
+    return await data.json() ?? {};
   }
 
   submitApplication(firstName: string, lastName: string, email: string) {
